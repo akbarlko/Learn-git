f40c1d1ec0a821920f9ccf8350d6b51181b9a0b4
akbarlko <akbarlko@gmail.com>
2022-08-17 17:01:44 +0530

    add multiply java file

---
+++ Multi.java
@@ -0,0 +1,9 @@
+public class Multi {
+        public static void main(String[] args){
+            int a = 5;
+            int b = 6;
+    
+            System.out.print("This is a product of" + (a*b));
+        }
+    
+}
